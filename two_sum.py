d44375df436d64e4e0bc60bc60b31dab23468146
Krzysztof Cieslik <krzysztofcieslik875@gmail.com>
2024-12-15 00:23:06 +0100

    add better solution

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,3 @@
-import numpy as np
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -6,19 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        array = np.array(nums)
-        idx_list = []
-        last_idx = None
-        while target > 0:
-            closest_idx = (np.abs(target - array)).argmin()
-            if not last_idx == closest_idx:
-                target = target - array[closest_idx]
-                last_idx = closest_idx
-                idx_list.append(closest_idx)
-            else:
-                np.delete(array,closest_idx) 
-                closest_idx = (np.abs(target - array)).argmin() +  1
-                target = target - array[closest_idx]
-                idx_list.append(closest_idx)
-        return idx_list
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
         
